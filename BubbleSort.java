c330bfbf86bbc048017eadb2bb15fa7f9a08094b
Priyanshu Rawat <100402236+Priyanshurawat007@users.noreply.github.com>
2023-10-13 22:52:00 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class BubbleSort {
+    public static void SortArr(int arr[]){
+        for(int turn=0;turn<arr.length-1;turn++){
+            for(int j=0;j<arr.length-1-turn;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+               }
+        }
+    }
+    }
+    public static void PrintArr(int arr[]){
+        for (int i = 0 ;i < arr.length;i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={5,4,1,2,3};
+        SortArr(arr);
+        PrintArr(arr);
+    }
+    
+}
