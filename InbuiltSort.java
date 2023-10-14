0f05a289a2e8e9af79b6fa34e31cd3e5f2b80b00
Priyanshu Rawat <100402236+Priyanshurawat007@users.noreply.github.com>
2023-10-14 22:32:53 +0530

    Add files via upload

--- InbuiltSort.java
+++ InbuiltSort.java
@@ -1,15 +1,27 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class InbuiltSort {
+    
 
     public static void PrintArr(int arr[]){
         for (int i = 0 ;i < arr.length;i++) {
-            System.out.print(arr[i]+" ");
+             System.out.print( arr[i]+" ");
         }
+        System.out.println("is Sorted Array");
+       
     }
     public static void main(String[] args) {
-        int arr[]={10,30,40,20,50,60};
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
 
+        System.out.println("Enter the array elements");
+        
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();       
+         }
         Arrays.sort(arr);
         PrintArr(arr);
 
