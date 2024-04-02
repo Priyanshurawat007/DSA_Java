0b58fdd1832f137e02e0c5e3e0e99d2d8458ed93
Priyanshu Rawat <100402236+Priyanshurawat007@users.noreply.github.com>
2024-04-02 23:02:41 +0530

    Add files via upload

--- Patterns.java
+++ Patterns.java
@@ -1,17 +1,62 @@
-package CodeWithHarry;
+import java.util.Scanner;
+public class Patterns {
+    public static void NForest(int n){
 
-public class Patterns{
-    public static void main(String[] args) {
-
-        for(int i=0;i<=4;i++){
-            for(int j=0;j<=5;j++){
-            System.out.print("*");
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
         }
-        System.out.println("*");
+    }
+    public static void Patterns2(int n){
+        for(int i=0;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void Patterns3(int n){
+        for (int i = 0; i <n; i++) {
+            for(int j=0;j<n-i;j++){
+                System.out.print("* ");
 
+            }
+            System.out.println();
+        }
     }
-        
+    public static void Patterns4(int n){
+        for(int i=0;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i <n; i++) {
+            for(int j=1;j<n-i;j++){
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
     }
-    
-    
+    public static void Patterns5(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number of Stars: ");
+        int n=sc.nextInt();
+        // NForest(n);
+        // Patterns2(n);
+        // Patterns3(n);
+        // Patterns4(n);
+        // Patterns5(n);
+    }
 }
